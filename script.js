2999ac367b3b15b62fa6d928679f83cce52243e2
Victor Galvao <victorgalvao900@gmail.com>
2022-05-06 14:47:14 -0300

    primeiro commit do projeto gerador de senhas

---
+++ script.js
@@ -0,0 +1,32 @@
+const sliderElement = document.querySelector("#slider");
+const buttonElement = document.querySelector("#button");
+
+const sizePassword = document.querySelector("#valor");
+const password = document.querySelector("#password");
+
+const containerPassword = document.querySelector("#container-password");
+
+const charset =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function () {
+  sizePassword.innerHTML = this.value;
+};
+
+function generatePassword() {
+  let pass = "";
+  for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copyPassword() {
+  navigator.clipboard.writeText(novaSenha);
+}
